456c4e99f4236a11bb3ee728060df3cfec628e62
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-11-08 14:20:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 mario = {}
 mario_file = \
     open("mario.csv")
+
 records = mario_file.readlines()
+
 for record in records:
     print(record.split(',', 1)[0])
     record = record.strip()
@@ -19,9 +21,13 @@ while True:
     if game2 not in mario:
         continue
     break
+
 score = float(mario[game][:3])*10
 score2 = float(mario[game2][:3])*10
-if score > score2:
+
+if score == score2:
+    print(f'{game} is just as good as {game2}')
+elif score > score2:
     print(f'{game} is {round(score / score2,3)} times better than {game2}')
 else:
     print(f'{game2} is {round(score2 / score,3)} times better than {game}')
